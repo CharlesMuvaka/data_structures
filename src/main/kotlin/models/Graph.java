b45e8576a9316daa1a9be44255b8cce3d550ce1e
CharlesMuvaka <muvakacharles@gmail.com>
2023-12-09 00:21:14 +0300

    define a blue print to initialise a graph instance.

---
+++ src/main/kotlin/models/Graph.java
@@ -0,0 +1,13 @@
+package models;
+
+public class Graph {
+    Node[] nodes;
+
+    public Graph(Node[] nodes){
+        this.nodes = nodes;
+    }
+
+    public Node[] getNodes() {
+        return nodes;
+    }
+}
